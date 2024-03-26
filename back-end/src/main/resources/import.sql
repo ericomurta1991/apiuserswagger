42857ddf8436b0a2e675622e550ce8a847b3d930
ericomurta1991 <erico_murta@yahoo.com.br>
2024-03-26 10:23:38 -0300

    added script to seed data to the database whenever the application is started.

---
+++ back-end/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user (name, age, email, created_At) VALUES ('fulano', 31, 'fulano@gmail.com', NOW());
+INSERT INTO tb_user (name, age, email, created_At) VALUES ('ciclano', 31, 'ciclano@gmail.com', NOW());
+INSERT INTO tb_user (name, age, email, created_At) VALUES ('beltrano', 31, 'beltrano@gmail.com', NOW());
+INSERT INTO tb_user (name, age, email, created_At) VALUES ('aquele', 31, 'aquele@gmail.com', NOW());
